65e42d2ea8c425b9ffe4975cd186695643094a22
Andrew Miller <andrew.miller@rakuten.com>
2018-04-05 10:50:20 +0900

    Correctly manage Apache configuration. Fixes #41.

--- phpswitch.sh
+++ phpswitch.sh
@@ -7,15 +7,17 @@ brew_prefix=$(brew --prefix | sed 's#/#\\\/#g')
 brew_array=("5.6","7.0","7.1","7.2")
 php_array=("php@5.6" "php@7.0" "php@7.1" "php@7.2")
 valet_support_php_version_array=("php@5.6" "php@7.0" "php@7.1" "php@7.2")
+latest_brew_php_version="7.2"
 php_installed_array=()
-php_version="php@$1"
-php_opt_path="$brew_prefix\/opt\/"
+given_php_version="$1"
+php_opt_path="$brew_prefix\/opt"
 
 php5_module="php5_module"
-apache_php5_lib_path="\/libexec\/apache2\/libphp5.so"
+apache_php5_lib_path="lib\/httpd\/modules\/libphp5.so"
 php7_module="php7_module"
-apache_php7_lib_path="\/libexec\/apache2\/libphp7.so"
-native_osx_php_apache_module="LoadModule php5_module libexec\/apache2\/libphp5.so"
+apache_php7_lib_path="lib\/httpd\/modules\/libphp7.so"
+native_osx_apache_php5_module="LoadModule php5_module libexec\/apache2\/libphp5.so"
+native_osx_apache_php7_module="LoadModule php7_module libexec\/apache2\/libphp7.so"
 
 php_module="$php5_module"
 apache_php_lib_path="$apache_php5_lib_path"
@@ -32,14 +34,21 @@ if [[ -z "$1" ]]; then
     exit
 fi
 
-if [ $(echo "$php_version" | sed 's/^php@//' | sed 's/\.//') -ge 70 ]; then
+# Formula for latest version of PHP doesn't inclue any @<version>
+brew_php_formula_to_link="php@$given_php_version"
+if [ "$given_php_version" == "$latest_brew_php_version" ]; then
+    brew_php_formula_to_link="php"
+fi
+
+
+if [ $(echo "$given_php_version" | sed 's/\.//') -ge 70 ]; then
     php_module="$php7_module"
     apache_php_lib_path="$apache_php7_lib_path"
 fi
 
 apache_change=1
 apache_conf_path="/etc/apache2/httpd.conf"
-apache_php_mod_path="$php_opt_path$php_version$apache_php_lib_path"
+apache_php_mod_path="$php_opt_path\/$given_php_version\/$apache_php_lib_path"
 
 valet_restart=0
 # Check if valet is already install
@@ -82,20 +91,20 @@ for i in ${php_array[*]}; do
 done
 
 # Check if php version support via valet
-if [[ (" ${valet_support_php_version_array[*]} " != *"$php_version"*) && ($valet_restart -eq 1) ]]; then
-    echo "Sorry, but $php_version is not support via valet" 2>&1
+if [[ (" ${valet_support_php_version_array[*]} " != *"$brew_php_formula_to_link"*) && ($valet_restart -eq 1) ]]; then
+    echo "Sorry, but $brew_php_formula_to_link is not support via valet" 2>&1
     exit 1
 fi
 
 # Check that the requested version is supported
-if [[ " ${php_array[*]} " != *"$php_version"* ]]; then
+if [[ " ${php_array[*]} " != *"$brew_php_formula_to_link"* ]]; then
     echo "Unknown version of PHP. PHP Switcher can only handle arguments of:" ${brew_array[@]} 2>&1
     exit 1
 fi
 
 # Check that the requested version is installed
-if [[ " ${php_installed_array[*]} " != *"$php_version"* ]]; then
-    echo "Sorry, but $php_version is not installed via brew. Install by running: brew install $php_version" 2>&1
+if [[ " ${php_installed_array[*]} " != *"$brew_php_formula_to_link"* ]]; then
+    echo "Sorry, but $brew_php_formula_to_link is not installed via brew. Install by running: brew install $brew_php_formula_to_link" 2>&1
     exit 1
 fi
 
@@ -106,14 +115,14 @@ if [[ ($valet_installed -eq 1) && ($valet_restart -eq 1) ]]; then
 fi
 
 # Switch Shell
-echo "Switching to $php_version"
+echo "Switching to $brew_php_formula_to_link"
 echo "Switching your shell"
 for i in ${php_installed_array[@]}; do
     if [[ -n $(brew ls --versions $i) ]]; then
         brew unlink $i
     fi
 done
-brew link --force "$php_version"
+brew link --force "$brew_php_formula_to_link"
 
 # Switch apache
 if [[ $apache_change -eq 1 ]]; then
@@ -127,7 +136,12 @@ if [[ $apache_change -eq 1 ]]; then
             loop_php_module="$php7_module"
             loop_apache_php_lib_path="$apache_php7_lib_path"
         fi
-        apache_module_string="LoadModule $loop_php_module $php_opt_path$j$loop_apache_php_lib_path"
+
+        if [ $(echo "$j" | sed 's/^php@//') == "$latest_brew_php_version" ]; then
+            j="php"
+        fi
+
+        apache_module_string="LoadModule $loop_php_module $php_opt_path\/$j\/$loop_apache_php_lib_path"
         comment_apache_module_string="#$apache_module_string"
 
         # If apache module string within apache conf
@@ -138,9 +152,15 @@ if [[ $apache_change -eq 1 ]]; then
             fi
         # Else the string for the php module is not in the apache config then add it
         else
-            sudo sed -i.bak "/$native_osx_php_apache_module/a\\
+            if grep -q "$native_osx_apache_php7_module" "$apache_conf_path"; then
+                sudo sed -i.bak "/$native_osx_apache_php7_module/a\\
 $comment_apache_module_string\\
 " $apache_conf_path
+            else
+                sudo sed -i.bak "/$native_osx_apache_php5_module/a\\
+$comment_apache_module_string\\
+" $apache_conf_path
+            fi
         fi
     done
     sudo sed -i.bak "s/\#LoadModule $php_module $apache_php_mod_path/LoadModule $php_module $apache_php_mod_path/g" $apache_conf_path
